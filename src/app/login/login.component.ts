35b67c831a2d40b14611a6a039f75b6e3ef09518
alsomeb <alsomeb@gmail.com>
2023-12-23 19:09:12 +0100

    Nav refactor styling & login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,37 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    RouterLink
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent implements OnInit{
-  ngOnInit(): void {
+  /*
+    This is a common pattern when working with reactive forms in Angular.
+    You start with an empty form group, and then use the FormBuilder to create the form controls
+    and add them to the form group.
+   */
+  loginForm: FormGroup = new FormGroup({});
+
+  constructor(private fb: FormBuilder) {
   }
 
+  ngOnInit(): void {
+    // FormBuilder
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(3)]]
+    });
+  }
+
+  handleLogin() {
+    console.log(this.loginForm);
+  }
 }
