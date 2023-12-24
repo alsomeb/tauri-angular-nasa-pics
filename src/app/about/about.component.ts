fed5d97ef986f09d49e97ff68b8ba01d92dcafc5
alsomeb <alsomeb@gmail.com>
2023-12-24 15:02:24 +0100

    About page styling, navbar style

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+    imports: [
+        NgOptimizedImage,
+        RouterLink
+    ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
