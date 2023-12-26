49b87457a32857a3c3fc59a8d18bfce801e80bc1
alsomeb <alsomeb@gmail.com>
2023-12-26 16:14:49 +0100

    - Register Comp + Form

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    RouterLink
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registerForm: FormGroup = new FormGroup({});
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    // FormBuilder
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(3)]]
+    });
+  }
+
+  handleRegister() {
+    console.log(this.registerForm);
+  }
 
 }
