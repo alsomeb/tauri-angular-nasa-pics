16b341043937e72ebe9c283ef256ce3354c077c8
alsomeb <alsomeb@gmail.com>
2023-12-29 17:27:30 +0100

    - Navbar logic to show correct menu if logged in or out - Todo, logout logic

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
 import {RouterLink, RouterLinkActive} from "@angular/router";
 import {invoke} from "@tauri-apps/api";
+import {SupaAuthService} from "../service/supa-auth.service";
 
 @Component({
   selector: 'app-navbar',
@@ -15,9 +16,21 @@ import {invoke} from "@tauri-apps/api";
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent implements OnInit {
+  isLoggedIn: boolean = false;
   sysTime: string = "-"
+
+
+  constructor(private authService: SupaAuthService) {
+  }
+
   ngOnInit(): void {
     this.getSystemTime();
+    this.checkIfLoggedIn();
+  }
+
+  checkIfLoggedIn() {
+    this.isLoggedIn = this.authService.isLoggedIn();
+    console.log(this.isLoggedIn);
   }
 
   getSystemTime() {
@@ -27,4 +40,4 @@ export class NavbarComponent implements OnInit {
   }
 }
 
-// TODO IF LOGGED IN DONT SHOW REGESITER / LOGIN, SHOW LOGOUT, IMPL LOGOUT LOGIC
+// TODO FIX LOG OUT AND CHECK NAVBAR IS CORRECT
