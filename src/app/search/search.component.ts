3437f66e1afdbf05800d769ab0eb9d142c1c537f
alsomeb <alsomeb@gmail.com>
2024-01-13 14:48:59 +0100

    - Search style somewhat done, under construction

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatIconModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
