b060f45e0c372cf8d75b21e720fa02f6e7b352bc
alsomeb <alsomeb@gmail.com>
2024-01-17 01:02:26 +0100

    - Rust Project restructure - Created pub mod http and http_functions.rs

---
+++ src-tauri/src/http/http_functions.rs
@@ -0,0 +1,56 @@
+use crate::{HTTP_CLIENT, RoverPic};
+
+#[tauri::command]
+pub async fn load_pic_by_date_async(date: String) -> Result<Vec<RoverPic>, String> {
+
+    if date.is_empty() {
+        return Err("Invalid date".to_string());
+    }
+
+    let api_key = std::env::var("NASA_API_KEY").unwrap_or_else(|_| "DEMO_KEY".to_string());
+
+    let url = format!(
+        "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date={}&api_key={}&page=1",
+        date, api_key
+    );
+
+    /*
+         - In Rust, the '?' operator is used for error propagation - propagate the error if there is one; otherwise, continue with the value.
+         - If used outside a function that returns Result or Option, the '?' operator cannot be used.
+
+         - This would be equivalent if not used '?' -> if the Result is an Err, it will short-circuit the function and return early with the error.
+
+            let result = match http_client.get(&url).send() {
+                Ok(value) => value,
+                Err(err) => return Err(err.to_string()),
+            };
+
+         - The await keyword is used to await the completion of the asynchronous future returned by http_client.get(&url).send().
+         - Once the future is resolved, the map_err function is applied to handle the conversion of potential errors.
+
+ */
+    let result = HTTP_CLIENT.get(&url).send().await.map_err(|err| err.to_string())?; // reuse lazy static HTTP CLIENT -> Not creating a new one with each Request
+
+
+    // Check if the response status is OK (200) or else Return Error msg
+    if result.status() != reqwest::StatusCode::OK {
+        return Err(format!("Request failed with status code: {}", result.status()));
+    }
+
+    // Deserialize JSON Response
+    let json_result: serde_json::Value = result.json().await.map_err(|err| err.to_string())?;
+
+    // Check if the "photos" array exists in the JSON response
+    let photos_array = json_result.get("photos").ok_or("Missing 'photos' array in the response")?
+        .as_array()
+        .cloned() // för att få den till Owned ist lånad Option Vec
+        .unwrap_or_default(); // För att få bort Option och bli Vec<Value>
+
+    // Convert to Vec<RoverPic>
+    // filter_map() behöver Option, då den mappar och filtrerar bort None's, samt from_value() -> Result som vi kan konvertera till en Option med ok()
+    let rover_pics = photos_array.iter()
+        .filter_map(|rover_pic| serde_json::from_value(rover_pic.clone()).ok())
+        .collect();
+
+    Ok(rover_pics)
+}
