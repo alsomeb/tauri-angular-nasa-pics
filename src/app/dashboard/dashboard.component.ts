8d72c322429f549bb4f1723331ad16efd5a1a9b4
alsomeb <alsomeb@gmail.com>
2024-02-27 19:56:12 +0100

    - Modal CTA Add to Album for Dashboard (Not done) - Added Reactive Form to capture which album to add picture in (Not done)

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,7 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {NasaRoverService, RoverPic} from "../service/nasa-rover.service";
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
-import {NgIf, NgOptimizedImage, NgStyle} from "@angular/common";
+import {NgForOf, NgIf, NgOptimizedImage, NgStyle} from "@angular/common";
 import {RouterLink, RouterLinkActive} from "@angular/router";
 import {Subscription} from "rxjs";
 import {open} from "@tauri-apps/api/shell";
@@ -10,75 +10,95 @@ import {NgxSkeletonLoaderModule} from "ngx-skeleton-loader";
 import {ProgressSpinnerModule} from "primeng/progressspinner";
 import {MatIconModule} from "@angular/material/icon";
 import {MatTooltipModule} from "@angular/material/tooltip";
+import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
-  selector: 'app-dashboard',
-  standalone: true,
-    imports: [MatCardModule, MatButtonModule, NgOptimizedImage, RouterLinkActive, RouterLink, NgIf, NgStyle, NgxSkeletonLoaderModule, ProgressSpinnerModule, MatIconModule, MatTooltipModule],
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+    selector: 'app-dashboard',
+    standalone: true,
+    imports: [MatCardModule, MatButtonModule, NgOptimizedImage, RouterLinkActive, RouterLink, NgIf, NgStyle, NgxSkeletonLoaderModule, ProgressSpinnerModule, MatIconModule, MatTooltipModule, ReactiveFormsModule, NgForOf],
+    templateUrl: './dashboard.component.html',
+    styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent implements OnInit, OnDestroy {
-  roverPics: RoverPic[] = []
-  isLoading: boolean = true;
-  currentSelectedRoverPic!: RoverPic;
-  currentSelectedRoverPicSub!: Subscription;
+    roverPics: RoverPic[] = []
+    isLoading: boolean = true;
+    currentSelectedRoverPic!: RoverPic;
+    currentSelectedRoverPicSub!: Subscription;
+    albumForm!: FormGroup;
+    albumOptions: string[] = ["Album 1", "Album 2", "Album 3"];
 
-  ngOnInit(): void {
-    this.fetchSamplePictures();
-    this.currentSelectedRoverPicSub = this.roverService.getCurrentSelectedRoverPic().subscribe((roverPic) => {
-      this.currentSelectedRoverPic = roverPic;
-    })
-  }
-
-  constructor(private roverService: NasaRoverService) {
-  }
-
-  async fetchSamplePictures() {
-    const monthAgo = this.getDateInPast(30);
-
-    try {
-       this.roverPics = await this.roverService.getRoverPicturesByDate(monthAgo);
-       this.isLoading = false;
-    } catch (error) {
-      this.isLoading = false;
-      console.error('Error fetching rover pictures:', error);
+    ngOnInit(): void {
+        this.fetchSamplePictures();
+        this.currentSelectedRoverPicSub = this.roverService.getCurrentSelectedRoverPic().subscribe((roverPic) => {
+            this.currentSelectedRoverPic = roverPic;
+        })
+        this.albumForm = this.fb.group({
+            albumSelect: [""]
+        })
     }
-  }
 
-  getDateInPast(amountDaysDeduct: number): string {
-    // Get the current date
-    const currentDate = new Date();
-
-    // Subtract X Days to collect desired Date in past
-    const date = new Date(currentDate);
-    date.setDate(currentDate.getDate() - amountDaysDeduct);
-
-    return date.toISOString().split("T")[0];
-  }
-
-  showMore(rover: RoverPic) {
-    this.roverService.setCurrentSelectedRoverPic(rover);
-  }
-
-  async showImageInSystemDefaultBrowser(imageUrl: string) {
-    try {
-      // opens the given URL on the default browser:
-      await open(imageUrl);
-    } catch (error) {
-      console.error(`Failed to open the URL '${imageUrl}' in the default browser.`, error);
+    constructor(private roverService: NasaRoverService, private fb: FormBuilder) {
     }
-  }
 
-  async downloadImage() {
-    try {
-      await this.roverService.downloadImage(this.currentSelectedRoverPic);
-    } catch (error) {
-      console.error(`Failed to download image'`, error);
+    async fetchSamplePictures() {
+        const monthAgo = this.getDateInPast(55);
+
+        try {
+            this.roverPics = await this.roverService.getRoverPicturesByDate(monthAgo);
+            this.isLoading = false;
+        } catch (error) {
+            this.isLoading = false;
+            console.error('Error fetching rover pictures:', error);
+        }
     }
-  }
 
-  ngOnDestroy(): void {
-    this.currentSelectedRoverPicSub.unsubscribe();
-  }
+    getDateInPast(amountDaysDeduct: number): string {
+        // Get the current date
+        const currentDate = new Date();
+
+        // Subtract X Days to collect desired Date in past
+        const date = new Date(currentDate);
+        date.setDate(currentDate.getDate() - amountDaysDeduct);
+
+        return date.toISOString().split("T")[0];
+    }
+
+    showMore(rover: RoverPic) {
+        this.roverService.setCurrentSelectedRoverPic(rover);
+    }
+
+    async showImageInSystemDefaultBrowser(imageUrl: string) {
+        try {
+            // opens the given URL on the default browser:
+            await open(imageUrl);
+        } catch (error) {
+            console.error(`Failed to open the URL '${imageUrl}' in the default browser.`, error);
+        }
+    }
+
+    async downloadImage() {
+        try {
+            await this.roverService.downloadImage(this.currentSelectedRoverPic);
+        } catch (error) {
+            console.error(`Failed to download image'`, error);
+        }
+    }
+
+    ngOnDestroy(): void {
+        this.currentSelectedRoverPicSub.unsubscribe();
+    }
+
+    handleAddToAlbum(roverPic: RoverPic) {
+        console.log(roverPic);
+        // Todo set current roverPic Behavior Subject so we can add it to album later
+    }
+
+    handleAlbumOptionSubmit() {
+        // todo add current roverPic to existing album
+        console.log(this.albumForm.value)
+    }
+
+    onAlbumSelectChange() {
+        this.handleAlbumOptionSubmit();
+    }
 }
